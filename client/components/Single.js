5c44760300418acc7a839521acb593eedc0354a3
henry le <henryle@henrys-MacBook-Pro.local>
2017-01-24 20:54:06 -0800

    add photo page

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        //index of the post
+
+        const { postId } = this.props.params
+        const i = this.props .posts.findIndex((post) => post.code === postId);
+        const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || [];
         return (
-            <div>
-                <h1> Single grid </h1>
+            <div className="single-photo">
+                <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} {...this.props}/>
             </div>
         )
     }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,7 +1,32 @@
 //a reducer takes in two things action and copy of current state
 
+function postComments (state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            return [
+                ...state.slice(0,action.i),
+                ...state.slice(action.i + 1)
+            ];
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments (state = [], action) {
-    console.log(state, action);
+    if(typeof action.postId !== 'undefined') {
+        return {
+            //take the current state
+            ...state,
+            //overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
